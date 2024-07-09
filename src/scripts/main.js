8262d7443d219d1fb74fa7f10bf2fbe4544334f8
jos3ph1205 <sharvinjoseph.e@gmail.com>
2024-07-09 19:00:02 +0800

    Few changes - test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,90 +1,28 @@
 /* --------------------------------------------------
 BTN CLICK ANIMATION
 -------------------------------------------------- */
-let buttons = document.querySelectorAll(".btn");
+const rippleBtn = document.querySelectorAll(".btn");
 
-function removeRipple(ripple, container) {
-   ripple.classList.replace("dwn", "up");
-
-   ripple.onanimationend = () => {
-      container.removeChild(ripple);
-   };
-}
-
-buttons.forEach((btn) => {
+rippleBtn.forEach((btn) => {
    let btnRipple = btn.appendChild(document.createElement("div"));
-
    btnRipple.classList.add("btn-ripple");
 
+   btn.oncontextmenu = (e) => e.preventDefault();
+
    btn.onmousedown = (e) => {
-      let rippleCount = btnRipple.children.length;
-      let mousedown = 0;
-
-      function mouseup(btn) {
-         return new Promise((resolve) => {
-            // btn.onmouseleave = () => {
-            //    resolve("up")
-            // }
-            window.onmouseup = () => {
-               if (mousedown > 0) {
-                  mousedown = 0;
-               }
-               // console.log(mousedown);
-               resolve("up");
-            };
-         });
-      }
-
-      mousedown++;
-      // console.log(mousedown);
-
-      let x = (e.offsetX / btn.offsetWidth) * 100,
-         y = (e.offsetY / btn.offsetHeight) * 100;
+      let cursorPos = {
+         x: e.offsetX,
+         y: e.offsetY,
+      };
 
       let ripple = btnRipple.appendChild(document.createElement("span"));
       ripple.classList.add("ripple");
-      ripple.classList.add("dwn");
 
-      ripple.style.left = `${x}%`;
-      ripple.style.top = `${y}%`;
+      ripple.style.left = `${cursorPos.x}px`;
+      ripple.style.top = `${cursorPos.y}px`;
 
-      window.onmouseup = () => {
-         if (mousedown > 0) {
-            mousedown = 0;
-         }
-         // console.log(mousedown);
-      };
-
-      window.oncontextmenu = () => {
-         if (mousedown > 0) {
-            mousedown = 0;
-         }
-         // console.log(mousedown);
-         console.log(rippleCount);
-         if (rippleCount > 1) {
-            (async function mouseState() {
-               await mouseup(btn);
-
-               removeRipple(btnRipple.children[0], btnRipple);
-               // console.log(mousedown);
-            })();
-         }
-      };
-
-      ripple.onanimationend = () => {
-         if (mousedown > 0) {
-            (async function mouseState() {
-               await mouseup(btn);
-
-               removeRipple(ripple, btnRipple);
-               // console.log(mousedown);
-            })();
-
-            return;
-         } else {
-            removeRipple(ripple, btnRipple);
-            // console.log(mousedown);
-         }
-      };
+      setTimeout(() => {
+         ripple.remove();
+      }, 600);
    };
 });
